dca190974c961b2ac5611db511b493a5b1613358
BD103 <59022059+BD103@users.noreply.github.com>
2023-11-02 11:51:01 -0400

    feat: play sound when duck right-clicked

--- src/main/kotlin/io/github/bd103/rubberduck/RubberDuck.kt
+++ src/main/kotlin/io/github/bd103/rubberduck/RubberDuck.kt
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.minecraft.item.ItemGroups
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.sound.SoundEvent
 import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
@@ -12,13 +13,15 @@ object RubberDuck : ModInitializer {
     private const val MOD_ID: String = "rubber_duck"
     private val logger = LoggerFactory.getLogger(MOD_ID)
 
+    val squeakSound: SoundEvent = SoundEvent.of(Identifier(MOD_ID, "squeak"))
+
     override fun onInitialize() {
         logger.info("Quack!")
 
         Registry.register(Registries.BLOCK, Identifier(MOD_ID, "rubber_duck"), RubberDuckBlock)
         Registry.register(Registries.ITEM, Identifier(MOD_ID, "rubber_duck"), RubberDuckItem)
+        Registry.register(Registries.SOUND_EVENT, Identifier(MOD_ID, "squeak"), squeakSound)
 
-        // Potentially also redstone category?
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(ItemGroupEvents.ModifyEntries {
             it.add(RubberDuckItem)
         })
--- src/main/kotlin/io/github/bd103/rubberduck/RubberDuckBlock.kt
+++ src/main/kotlin/io/github/bd103/rubberduck/RubberDuckBlock.kt
@@ -3,19 +3,31 @@ package io.github.bd103.rubberduck
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.block.MapColor
 import net.minecraft.block.ShapeContext
+import net.minecraft.block.piston.PistonBehavior
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemPlacementContext
+import net.minecraft.sound.BlockSoundGroup
+import net.minecraft.sound.SoundCategory
 import net.minecraft.state.StateManager
 import net.minecraft.state.property.Properties
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Hand
+import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
+import net.minecraft.world.World
 
 private val settings = FabricBlockSettings.create()
     .nonOpaque()
+    .sounds(BlockSoundGroup.WOOL)
     .strength(0.2F)
+    .mapColor(MapColor.YELLOW)
+    .pistonBehavior(PistonBehavior.DESTROY)
 
 object RubberDuckBlock : Block(settings) {
     init {
@@ -65,5 +77,29 @@ object RubberDuckBlock : Block(settings) {
                 11.0 / 16.0, 8.0 / 16.0, 11.0 / 16.0,
             )
         }
+    }
+
+    // Play squeak noise when right-clicked
+    override fun onUse(
+        state: BlockState?,
+        world: World?,
+        pos: BlockPos?,
+        player: PlayerEntity?,
+        hand: Hand?,
+        hit: BlockHitResult?
+    ): ActionResult {
+        // Sound events are played on the server
+        if (!world!!.isClient) {
+            world.playSound(
+                null,
+                pos,
+                RubberDuck.squeakSound,
+                SoundCategory.BLOCKS,
+                1.0F,
+                1.0F,
+            )
+        }
+
+        return ActionResult.SUCCESS
     }
 }
