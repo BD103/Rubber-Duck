114de4c2a58acdda9e5092a465cc2cf02af2d136
BD103 <59022059+BD103@users.noreply.github.com>
2023-11-03 14:08:55 -0400

    feat: rubber ducks are wearable
    Closes #1

--- src/main/kotlin/io/github/bd103/rubberduck/RubberDuckItem.kt
+++ src/main/kotlin/io/github/bd103/rubberduck/RubberDuckItem.kt
@@ -1,9 +1,16 @@
 package io.github.bd103.rubberduck
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.entity.EquipmentSlot
 import net.minecraft.item.BlockItem
+import net.minecraft.item.Equipment
+import net.minecraft.sound.SoundEvent
+import net.minecraft.sound.SoundEvents
 import net.minecraft.util.Rarity
 
 private val settings = FabricItemSettings().rarity(Rarity.UNCOMMON)
 
-object RubberDuckItem : BlockItem(RubberDuckBlock, settings)
+object RubberDuckItem : BlockItem(RubberDuckBlock, settings), Equipment {
+    override fun getSlotType() = EquipmentSlot.HEAD
+    override fun getEquipSound(): SoundEvent = SoundEvents.ITEM_ARMOR_EQUIP_LEATHER
+}
