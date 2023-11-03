03e7d071fc28cd6141194c726dbc83c04b8bf16f
BD103 <59022059+BD103@users.noreply.github.com>
2023-11-02 21:56:11 -0400

    feat: 10% chance of finding a rubber duck in a dungeon

--- src/main/kotlin/io/github/bd103/rubberduck/RubberDuck.kt
+++ src/main/kotlin/io/github/bd103/rubberduck/RubberDuck.kt
@@ -2,7 +2,12 @@ package io.github.bd103.rubberduck
 
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents
 import net.minecraft.item.ItemGroups
+import net.minecraft.loot.LootPool
+import net.minecraft.loot.LootTables
+import net.minecraft.loot.entry.EmptyEntry
+import net.minecraft.loot.entry.ItemEntry
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.sound.SoundEvent
@@ -22,8 +27,21 @@ object RubberDuck : ModInitializer {
         Registry.register(Registries.ITEM, Identifier(MOD_ID, "rubber_duck"), RubberDuckItem)
         Registry.register(Registries.SOUND_EVENT, Identifier(MOD_ID, "squeak"), squeakSound)
 
+        // Add rubber duck to item group
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(ItemGroupEvents.ModifyEntries {
             it.add(RubberDuckItem)
+        })
+
+        // Modify dungeon loot table
+        LootTableEvents.MODIFY.register(LootTableEvents.Modify { _, _, id, tableBuilder, source ->
+            if (source.isBuiltin && id.equals(LootTables.SIMPLE_DUNGEON_CHEST)) {
+                tableBuilder.pool(
+                    // 10% chance of rolling a duck
+                    LootPool.builder()
+                        .with(ItemEntry.builder(RubberDuckItem).weight(1))
+                        .with(EmptyEntry.builder().weight(9))
+                )
+            }
         })
     }
 }
