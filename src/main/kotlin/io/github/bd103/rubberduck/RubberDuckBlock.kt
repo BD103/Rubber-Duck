eb9dde9f57dd135c95a313969752ca9b7d651a6d
BD103 <59022059+BD103@users.noreply.github.com>
2023-11-13 22:23:03 -0500

    fix: make rubber ducks waterloggable
    Closes #6

--- src/main/kotlin/io/github/bd103/rubberduck/RubberDuckBlock.kt
+++ src/main/kotlin/io/github/bd103/rubberduck/RubberDuckBlock.kt
@@ -1,12 +1,11 @@
 package io.github.bd103.rubberduck
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
-import net.minecraft.block.Block
-import net.minecraft.block.BlockState
-import net.minecraft.block.MapColor
-import net.minecraft.block.ShapeContext
+import net.minecraft.block.*
 import net.minecraft.block.piston.PistonBehavior
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.fluid.FluidState
+import net.minecraft.fluid.Fluids
 import net.minecraft.item.ItemPlacementContext
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.sound.SoundCategory
@@ -21,6 +20,7 @@ import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
+import net.minecraft.world.WorldAccess
 
 private val settings = FabricBlockSettings.create()
     .nonOpaque()
@@ -29,30 +29,57 @@ private val settings = FabricBlockSettings.create()
     .mapColor(MapColor.YELLOW)
     .pistonBehavior(PistonBehavior.DESTROY)
 
-object RubberDuckBlock : Block(settings) {
+object RubberDuckBlock : Block(settings), Waterloggable {
     init {
         // Set the default rotation to north
-        defaultState = defaultState.with(Properties.HORIZONTAL_FACING, Direction.NORTH)
+        defaultState = defaultState
+            .with(Properties.HORIZONTAL_FACING, Direction.NORTH)
+            .with(Properties.WATERLOGGED, false)
     }
 
-    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>?) {
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
         // Configure the duck to have a rotation property
-        builder?.add(Properties.HORIZONTAL_FACING)
+        builder.add(Properties.HORIZONTAL_FACING, Properties.WATERLOGGED)
     }
 
-    override fun getPlacementState(ctx: ItemPlacementContext?): BlockState? {
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
         // Make the duck rotate to face the player when placed
-        return super.getPlacementState(ctx)?.with(Properties.HORIZONTAL_FACING, ctx?.horizontalPlayerFacing?.opposite)
+        return super.getPlacementState(ctx)!!
+            .with(Properties.HORIZONTAL_FACING, ctx.horizontalPlayerFacing?.opposite)
+            .with(Properties.WATERLOGGED, ctx.world.getFluidState(ctx.blockPos).isOf(Fluids.WATER))
+    }
+
+    // Render water overtop of duck when waterlogged
+    override fun getFluidState(state: BlockState): FluidState = if (state.get(Properties.WATERLOGGED)) {
+        Fluids.WATER.getStill(false)
+    } else {
+        super.getFluidState(state)
+    }
+
+    // Make water flow when neighbor blocks are broken
+    override fun getStateForNeighborUpdate(
+        state: BlockState,
+        direction: Direction,
+        neighborState: BlockState,
+        world: WorldAccess,
+        pos: BlockPos,
+        neighborPos: BlockPos,
+    ): BlockState {
+        if (state.get(Properties.WATERLOGGED)) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world))
+        }
+
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos)
     }
 
     // Return the correct outline depending on rotation
     override fun getOutlineShape(
-        state: BlockState?,
-        world: BlockView?,
-        pos: BlockPos?,
-        context: ShapeContext?
+        state: BlockState,
+        world: BlockView,
+        pos: BlockPos,
+        context: ShapeContext,
     ): VoxelShape {
-        val direction = state?.get(Properties.HORIZONTAL_FACING) ?: return VoxelShapes.fullCube()
+        val direction = state.get(Properties.HORIZONTAL_FACING) ?: return VoxelShapes.fullCube()
 
         // TODO(BD103): More elegant solution than hardcoding duck outlines
         return when (direction) {
@@ -81,15 +108,15 @@ object RubberDuckBlock : Block(settings) {
 
     // Play squeak noise when right-clicked
     override fun onUse(
-        state: BlockState?,
-        world: World?,
-        pos: BlockPos?,
-        player: PlayerEntity?,
-        hand: Hand?,
-        hit: BlockHitResult?
+        state: BlockState,
+        world: World,
+        pos: BlockPos,
+        player: PlayerEntity,
+        hand: Hand,
+        hit: BlockHitResult,
     ): ActionResult {
         // Sound events are played on the server
-        if (!world!!.isClient) {
+        if (!world.isClient) {
             world.playSound(
                 null,
                 pos,
